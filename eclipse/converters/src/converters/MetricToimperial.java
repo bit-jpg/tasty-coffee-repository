650b5040c632560d12eca0491fd261ea73f5d019
jaydenPC <176967879+bit-jpg@users.noreply.github.com>
2025-09-08 19:27:56 +0800

    Attemp 2

--- eclipse/converters/src/converters/MetricToimperial.java
+++ eclipse/converters/src/converters/MetricToimperial.java
@@ -3,12 +3,17 @@ package converters;
 import java.util.Scanner;
 
 public class MetricToimperial {
-    public static void main(String[] args);
+    public static void main(String[] args){
     double cm, inch, feet;
     Scanner input = new Scanner(System.in);
     
     System.out.print("Centimeter: ");
     cm = input.nextDouble();
+    input.close();
 
-    
+    feet = cm / 2.54;
+    inch = cm / 30.48;
+    System.out.format("%10s %8s", "Feet: " +feet, "\n");
+    System.out.format("%10s %8s", "Inch: " +inch, "\n");
+    }
 }
