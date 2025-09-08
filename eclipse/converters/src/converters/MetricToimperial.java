584bf7ffbc6178d1980dec1355a7af3b4c40b391
jaydenPC <176967879+bit-jpg@users.noreply.github.com>
2025-09-08 19:18:58 +0800

    add new file

---
+++ eclipse/converters/src/converters/MetricToimperial.java
@@ -0,0 +1,14 @@
+package converters;
+
+import java.util.Scanner;
+
+public class MetricToimperial {
+    public static void main(String[] args);
+    double cm, inch, feet;
+    Scanner input = new Scanner(System.in);
+    
+    System.out.print("Centimeter: ");
+    cm = input.nextDouble();
+
+    
+}
